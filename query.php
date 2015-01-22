2eab259739f72c46798eeddc350e7d66408c9da9
PabloHenriqueLima <pabllo_sjp@hotmail.com>
2015-01-22 09:23:45 -0300

    news news

--- query.php
+++ query.php
@@ -1,7 +1,12 @@
 <?php
 /** Jesus Cristo - O Senhor e Salvador da Terra. **/
-require_once('configs/localMysql.php');
+require_once('configs/MysqlChange.php');
+$con = new MysqlChange();
 
-$q = "UPDATE entrada SET finalizado = ' '";
-$sql = $mysqli->prepare($q);
-$sql->execute();
+$con->changeAll('dbmy0053.whservidor.com','globoinfor1','GloboSec2415','globoinfor1');
+$mysqli = $con->connect();
+
+
+$q = "UPDATE entrada SET statusServico = 'Serviço em Andamento'";
+if(!$sql = $mysqli->prepare($q)) echo $mysqli->error;
+if(!$sql->execute()) echo $mysqli->error;
